0acafbcf343a5f4ed84b2b976caf46292bf2e972
Arslan Ali <sp19-bcs-103@cuilahore.edu.pk>
2019-12-13 18:48:48 +0500

    Update addstudent.c

--- addstudent.c
+++ addstudent.c
@@ -9,32 +9,44 @@ int main(void)
     char student_id[30];
     char s_registration[30] = "";
     char student_name[30] = "";
-    int position;
-    int position2;
+    int position = 30;
+    int position2 =90;
+    int position3;
     int sr_lenght;
     int sn_lenght;
+    char dump[3];
     fp = fopen("data.txt","r+");
     if (fp==NULL)
     {
         printf("Error");
     }
-    int i=1,i2=-1,j;
-    char c;
-    while (1)
+    fseek(fp,-2,2);
+    fgets(dump,3,fp);
+	int n = atoi(dump);
+    printf("%d",n);
+    int number = n;
+    int i=1,i2=-1;
+    while (number>0)
     {
         rewind(fp);
-        position2 =(210*i)-120+i2;
-        fseek(fp,position2,0);
-        fgets(student_name,2,fp);
-        if (student_name[0]!="0")
+        position=(220*i)-180+i2;
+        fseek(fp,position,0);
+        fgets(s_registration,30,fp);
+                if (s_registration[0]=='0')
         {
-            i++;i2++;
-        }
-        else
-        {
-            i++;i2++;
             break;
         }
-
+        printf("Enter the Record for this registration no %s\n",s_registration);
+        gets(student_name);
+        position2 =(220*i)-120+i2;
+        fseek(fp,position2,0);
+        fputs(student_name,fp);
+        position3 =(220*(i+n))-120+(i2+n);
+        fseek(fp,position3,0);
+        fputs(student_name,fp);
+        i++;i2++;
+        number--;
+        //printf("%d",position2);
     }
+    fclose(fp);
 }
