3b965ee1f7fdeb9f244e0b69e8c22c81f931b567
Arslan Ali <sp19-bcs-103@cuilahore.edu.pk>
2019-12-14 19:54:31 +0500

    Update attendence.c

--- attendence.c
+++ attendence.c
@@ -21,9 +21,8 @@ int main(void)
     }
     fseek(fp,0,0);
     int n,n2;
-    fscanf(fp,"%d %d",&n,&n2);
+    fscanf(fp,"%d",&n);
     int s_number = n;
-    n2 = n;
     int i=2,i2=1;
     int dum=10;
     while (1)
@@ -34,24 +33,36 @@ int main(void)
         fgets(s_registration,15,fp);
         if (!strcmp(t_id,s_registration))
         {
-            i2--;
-            printf("NAME                                         REGISTRATION NO       ATTEDENCE");
-            for (;n>0;n--)
+            int j,n_a=60;
+            int dump1 = i;
+            int dump2 = --i2;
+            printf("Enter Attendance\n");
+            printf("Enter the number of Attendance do you want to enter:  ");
+            scanf("%d",&j);
+            for (;j>0;j--)
             {
-                rewind(fp);
-                position=(280*i)-240+i2;
-                fseek(fp,position,0);
-                fgets(student_r,20,fp);
-                position=(280*i)-179+i2;
-                fseek(fp,position,0);
-                fgets(studen_name,20,fp);
-                printf("\n%-45s%-25s",studen_name,student_r);
-                position=(280*i)-60+i2;
-                scanf("%s",attadence);
-                fseek(fp,position,0);
-                fputs(attadence,fp);
-                i++;i2++;
+                printf("NAME                                         REGISTRATION NO       ATTEDENDANCE");
+                i = dump1; i2 = dump2;
+                for (n=s_number;n>0;n--)
+                {
+                    rewind(fp);
+                    position=(280*i)-240+i2;
+                    fseek(fp,position,0);
+                    fgets(student_r,20,fp);
+                    position=(280*i)-179+i2;
+                    fseek(fp,position,0);
+                    fgets(studen_name,20,fp);
+                    printf("\n%-45s%-25s",studen_name,student_r);
+                    position=(280*i)-n_a+i2;
+                    scanf("%s",attadence);
+                    fseek(fp,position,0);
+                    fputs(attadence,fp);
+                    i++;i2++;
+                }
+                system("cls");
+                n_a -=2;
             }
+
             break;
         }
         else if (s_registration[0]=='0')
