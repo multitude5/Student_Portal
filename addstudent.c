818414b388f052d6cccc0bdbbca36ed0e241f816
Arslan Ali <sp19-bcs-103@cuilahore.edu.pk>
2019-12-17 21:38:24 +0500

    Update addstudent.c

--- addstudent.c
+++ addstudent.c
@@ -1,121 +1,53 @@
 #include<stdio.h>
 #include<string.h>
 #include<conio.h>
-#include<windows.h>
-void gotoxy(int x, int y);
+
 int main(void)
 {
     FILE *fp;
-    int x,y;
+    char teacher_id[30];
+    char student_id[30];
+    char s_registration[30] = "";
+    char student_name[30] = "";
+    int position = 30;
+    int position2 =90;
+    int position3;
+    int sr_lenght;
+    int sn_lenght;
     char dump[3];
-    char t_id[15]="ifthar@cui.com";
-    char s_registration[15];
-    char student_r[20];
-    char studen_name[20];
-    char attadence[1];
+    int n1,n2;
     fp = fopen("data.txt","r+");
-    int position;
     if (fp==NULL)
     {
         printf("Error");
     }
     fseek(fp,0,0);
-    int n,n2,n_a;;
-    fscanf(fp,"%d %d %d",&n,&n2,&n_a);
-    int s_number = n;
+    fscanf(fp,"%d %d",&n1,&n2);
+    printf("%d \n%d",n1,n2);
+    int s_number = n1;
+    int n;
+    int number2= n2;
     int i=2,i2=1;
-    int dum=0;
-    while (1)
+    while (s_number>0)
     {
         rewind(fp);
-        position=(280*i)-280+i2;
+        position=(280*i)-240+i2;
         fseek(fp,position,0);
-        fgets(s_registration,15,fp);
-        //printf("%d",s_registration);
-        if (!strcmp(t_id,s_registration))
-        {
-            i2--;
-            int k;
-            gotoxy(40,1);
-            printf("Press One For Attendance");
-            gotoxy(35,2);
-            printf("Press Two For Entering Marks\n");
-            gotoxy(39,3);
-            scanf("%d",&k);
-            if (k==1)
-            {
-                printf("Enter Attendance\n");
-                printf("NAME                                         REGISTRATION NO       ATTEDENDANCE");
-                while (1)
-                {
-                    rewind(fp);
-                    position=(280*i)-280+(i2+1);
-                    fseek(fp,position,0);
-                    fgets(s_registration,15,fp);
-                    if (strcmp(t_id,s_registration))
-                    {
-                        break;
-                    }
-                    position=(280*i)-240+i2;
-                    fseek(fp,position,0);
-                    fgets(student_r,20,fp);
-                    position=(280*i)-179+i2;
-                    fseek(fp,position,0);
-                    fgets(studen_name,20,fp);
-                    printf("\n%-45s%-25s",studen_name,student_r);
-                    position=(280*i)-n_a+i2;
-                    scanf("%s",attadence);
-                    fseek(fp,position,0);
-                    fputs(attadence,fp);
-                    i++;i2++;
-                }
-                system("cls");
-                n_a -=2;
-                rewind(fp);
-                fprintf(fp,"%d %d %d",n,n2,n_a);
-                break;
-            }
-            else if (k=2)
-            {
-                printf("Enter the marks!");
-                int s=1;
-                while (1)
-                {
-                    rewind(fp);
-                    position=(280*i)-280+(i2+1);
-                    fseek(fp,position,0);
-                    fgets(s_registration,15,fp);
-                    if (strcmp(t_id,s_registration))
-                    {
-                        break;
-                    }
-                    position=(280*i)-240+i2;
-                    fseek(fp,position,0);
-                    fgets(student_r,20,fp);
-                    position=(280*i)-179+i2;
-                    fseek(fp,position,0);
-                    fgets(studen_name,20,fp);
-                    printf("\n%-2d %-45s%-25s",s,studen_name,student_r);
-                    s++;i++;i2++;
+        printf("%d",ftell(fp));
+        fgets(s_registration,20,fp);
 
-                }
-            }
-        }
-        else if (s_registration[0]=='0')
+        printf("\nEnter the Record for this registration no %s: ",s_registration);
+        gets(student_name);
+        n=0;
+        for (number2=n2;number2>0;number2--)
         {
-            break;
+            position3 =(280*(i+n))-180+(i2+n);
+            fseek(fp,position3,0);
+            fputs(student_name,fp);
+            n = n1+n;
         }
-        i++;i2++;dum++;
+        i++;i2++;
+        s_number--;
     }
-}
-
-
-
-
-void gotoxy(int x, int y)
-{
-    COORD c;
-    c.X=x;
-    c.Y=y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
+    fclose(fp);
 }
